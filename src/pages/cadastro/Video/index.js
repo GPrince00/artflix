cb66bda9851b6908361a05f69df71b5bd556bff6
GPrince00 <gustavoprince0@gmail.com>
2020-08-05 22:51:16 -0300

    feat(link): updates the link

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function Video() {
   return (
     <PageDefault>
-        Pagina de cadastro
+        <h1>Cadastro de Video</h1>
+
+        <Link to="/cadastro/categoria">
+            Cadastrar Categoria
+        </Link>
     </PageDefault>
   );
 }
