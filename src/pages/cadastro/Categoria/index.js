cd976d905b559d199cb567bf2e8baca1a0a3879d
GPrince00 <gustavoprince0@gmail.com>
2020-09-01 22:49:39 -0300

    feat(categorias): adds state and onChange for input text

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('');
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
       <form>
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value={nomeDaCategoria}
+            onChange={(e) => {setNomeDaCategoria(e.target.value)}}
           />
         </label>
 
