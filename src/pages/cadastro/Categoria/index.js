b2200d44c0f329604c3218bbbb9741cf77d05ccf
GPrince00 <gustavoprince0@gmail.com>
2020-09-01 23:02:17 -0300

    feat(categorias): adds a submit on form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,12 +3,19 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(["Teste"]);
   const [nomeDaCategoria, setNomeDaCategoria] = useState('');
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(e) {
+        e.preventDefault();
+        setCategorias([
+            ...categorias,
+            nomeDaCategoria
+        ]);
+      }}>
         <label>
           Nome da Categoria:
           <input
@@ -23,7 +30,15 @@ function CadastroCategoria() {
         </button>
       </form>
 
-
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
       <Link to="/">
         Ir para home
       </Link>
